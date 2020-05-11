3372a6850b12ea6b8925096803fd9b7261ab4c62
Bailey Le <baileyble@gmail.com>
2020-05-10 23:40:51 -0400

    Fixed package.json

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import $ from 'jquery';
 import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render(
