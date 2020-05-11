be81c43e456e95e01921cf82fc28f38c6e4d2414
Bailey Le <baileyble@gmail.com>
2020-05-10 21:56:52 -0400

    Created TransactionTable component

---
+++ src/components/TransactionTableComponent.js
@@ -0,0 +1,33 @@
+import React, {Component} from "react";
+import { Table } from 'reactstrap'
+import Header from "./HeaderComponent";
+
+function TransactionTable({transactions}) {
+    return (
+        <>
+            <div className="container">
+                <Table>
+                    <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Transaction</th>
+                        <th>Memo</th>
+                        <th>Amount</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {transactions.map(transaction => <tr key={transaction.Id}>
+                        <td>{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(transaction.Created)))}</td>
+                        <td>{transaction.Type}</td>
+                        <td>{transaction.Memo}</td>
+                        <td>${transaction.Amount}</td>
+                    </tr>)}
+                    </tbody>
+                </Table>
+            </div>
+        </>
+    )
+}
+
+
+export default TransactionTable
