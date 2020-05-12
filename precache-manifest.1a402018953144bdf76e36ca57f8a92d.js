c94cf65cca51ea9d8771d53329c84060546d5b00
Bailey Le <baileyble@gmail.com>
2020-05-11 21:25:27 -0400

    Updates

---
+++ precache-manifest.1a402018953144bdf76e36ca57f8a92d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f08dc9940af96fb9c60cdea95d423507",
+    "url": "/finance-portal/index.html"
+  },
+  {
+    "revision": "2894d952aff4c82556a7",
+    "url": "/finance-portal/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "92f1f27d8e13e50852e8",
+    "url": "/finance-portal/static/css/main.3b410825.chunk.css"
+  },
+  {
+    "revision": "2894d952aff4c82556a7",
+    "url": "/finance-portal/static/js/2.83bcf641.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/finance-portal/static/js/2.83bcf641.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92f1f27d8e13e50852e8",
+    "url": "/finance-portal/static/js/main.800e578e.chunk.js"
+  },
+  {
+    "revision": "72459060e62fb1f43771",
+    "url": "/finance-portal/static/js/runtime-main.9334952a.js"
+  }
+]);
